5af23b5fd239c0ebafef45e0c33bc45d59dfbecb
dangtuanthanh <dangtuanthanh265@gmail.com>
2024-12-07 08:54:55 +0700

    fixCors3

--- routes/index.js
+++ routes/index.js
@@ -117,7 +117,7 @@ router.post('/login', async function (req, res) {
       res.cookie('refreshToken', refreshToken, {
         httpOnly: true,   // Chỉ cho phép HTTP truy cập, không cho JavaScript
         secure: true,    // Tạm thời không bật chế độ bảo mật HTTPS
-        sameSite: 'None', // Không gửi cookie với các request ngoài cùng nguồn
+        sameSite: 'Lax', // Không gửi cookie với các request ngoài cùng nguồn
         maxAge: 7 * 24 * 60 * 60 * 1000 // Thời gian sống 7 ngày
       });
 
@@ -220,7 +220,7 @@ router.post('/logout', async function (req, res) {
       res.cookie('refreshToken', '', {
         httpOnly: true,
         secure: true, // Nếu triển khai thực tế, nên đặt thành true khi sử dụng HTTPS
-        sameSite: 'None',
+        sameSite: 'Lax',
         maxAge: 0 // Xóa cookie ngay lập tức
       });
 
